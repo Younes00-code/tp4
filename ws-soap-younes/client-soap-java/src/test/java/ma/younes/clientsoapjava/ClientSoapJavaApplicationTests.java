5c2fe4c017509112992763a6639fc72fdb6f1d17
Younes00-code <youn.ziani@edu.umi.ac.ma>
2025-05-27 21:24:18 +0100

    Add files via upload

---
+++ ws-soap-younes/client-soap-java/src/test/java/ma/younes/clientsoapjava/ClientSoapJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.younes.clientsoapjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSoapJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ws-soap-younes/src/test/java/ma/younes/wssoapyounes/WsSoapYounesApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.younes.wssoapyounes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSoapYounesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
